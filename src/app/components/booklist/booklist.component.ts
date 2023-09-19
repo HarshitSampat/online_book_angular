05577a9a3dce23a9a3442b20ad0284ec43a39e2e
HarshitSampat <60622363+HarshitSampat@users.noreply.github.com>
2023-09-19 17:41:02 +0530

    1 file modified, 1 added, 1 deleted

--- src/app/components/booklist/booklist.component.ts
+++ src/app/components/booklist/booklist.component.ts
@@ -1,14 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { Book } from '../../book/book.model';
 // import { BookstoreInventoryService } from '../../services/bookstore-inventory.service';
-import { BookService } from 'src/app/services/book.service';
-import { CartService } from 'src/app/services/Cart.service';
-
+import { BookService } from '../../services/book.service';
+import { CartService } from '../../services/Cart.service';
 
 @Component({
   selector: 'app-booklist',
   templateUrl: './booklist.component.html',
-  styleUrls: ['./booklist.component.css']
+  styleUrls: ['./booklist.component.css'],
 })
 export class BooklistComponent implements OnInit {
   books: any[] = [];
@@ -16,7 +15,7 @@ export class BooklistComponent implements OnInit {
   constructor(
     private bookService: BookService,
     private cartService: CartService
-    ) {}
+  ) {}
 
   ngOnInit(): void {
     this.bookService.getBooks().subscribe((data) => {
@@ -26,14 +25,13 @@ export class BooklistComponent implements OnInit {
   updateCartItems() {
     this.cartItems = Object.values(this.cartService.getCartItems());
     console.log(this.cartItems);
-    
   }
-  getTitle(Item : any){
+  getTitle(Item: any) {
     console.log(Item);
-    
-    return Item.title
+
+    return Item.title;
   }
-  getQuty(Item:any){
-    return Item.quantity
+  getQuty(Item: any) {
+    return Item.quantity;
   }
 }
