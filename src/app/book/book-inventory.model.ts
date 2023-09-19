86fee9d9be509de2569d6191d9438aea4d7d09b7
HarshitSampat <60622363+HarshitSampat@users.noreply.github.com>
2023-09-19 18:01:59 +0530

    2 files deleted

--- src/app/book/book-inventory.model.ts
+++
@@ -1,7 +0,0 @@
-import { Book } from './book.model';
-
-export interface BookInventory {
-  getBooks(): Book[];
-  getBookByTitle(title: string): Book | undefined;
-  placeOrder(title: string, quantity: number): boolean;
-}
--- src/app/book/book.model.ts
+++
@@ -1,9 +0,0 @@
-export class Book {
-    constructor(
-      public title: string,
-      public author: string,
-      public price: number,
-      public stock: number,
-      public image: string
-    ) {}
-  }
